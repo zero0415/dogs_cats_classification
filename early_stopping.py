f091212b0834bb3fe14219db89ce667897431bd5
zero0415 <zero0415@gmail.com>
2024-02-02 07:27:04 +0800

    Update early_stopping.py

--- early_stopping.py
+++ early_stopping.py
@@ -1,15 +1,17 @@
 import torch
 
 class EarlyStopping:
-    def __init__(self, patience=5, delta=0, checkpoint_path='early_stop_checkpoint.pth'):
+    def __init__(self, patience=5, delta=0, checkpoint_path='early_stop_checkpoint.pth', log_file=None):
         self.patience = patience
         self.delta = delta
         self.counter = 0
         self.best_score = None
         self.early_stop = False
         self.checkpoint_path = checkpoint_path
+        self.log_file = log_file
 
     def __call__(self, val_loss, model):
+        print(f"EarlyStopping val_loss: {val_loss} vs best_score: {self.best_score}", file=self.log_file)
         if self.best_score is None:
             self.best_score = val_loss
             self.save_checkpoint(val_loss, model)
@@ -24,4 +26,4 @@ class EarlyStopping:
 
     def save_checkpoint(self, val_loss, model):
         torch.save(model.state_dict(), self.checkpoint_path)
-        print(f'Model saved! Validation loss decreased ({self.best_score:.6f} --> {val_loss:.6f})')
+        print(f'Model saved! Validation loss decreased ({self.best_score:.6f} --> {val_loss:.6f})', file=self.log_file)
